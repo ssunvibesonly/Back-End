84534b681c52e8174972da3b4c5efc83eda3d37a
ssunvibesonly <wlwlgoyo@naver.com>
2024-02-06 23:45:55 +0900

    variable240206

---
+++ JAVA/JavaBook/src/ch2/VarEx3.java
@@ -0,0 +1,14 @@
+package ch2;
+
+public class VarEx3 {
+
+	public static void main(String[] args) {
+		final int score=100;
+		
+//		score=200;		
+//		score는 상수이므로 값 변경 불가능
+  
+		System.out.println(score);
+	}
+
+}
