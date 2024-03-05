cf5982f16a9f1c634b650f577c8de717229d4b9b
intellicode-sy <goyoung@intellicode.co.kr>
2024-03-05 17:59:48 +0900

    240305-company

--- Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package hello.hellospring;
 
 import hello.hellospring.Service.MemberService;
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +24,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Spring/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
 import hello.hellospring.Service.MemberService;
+import hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 //스프링 컨테이너에 @Controller가 있으면 MemberController 객체를 생성해서 넣어둠(갖고있는다.)
 //스프링 컨테이너가 관리
@@ -18,5 +24,27 @@ public class MemberController {
     public MemberController(MemberService memberService) {
 
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm"; //ViewResolver를 통해 웹 어플리케이션 화면(뷰/ 템플릿)을 찾아준다.
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member=new Member();
+        member.setName(form.getName());
+
+        //System.out.println("member: "+member.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
