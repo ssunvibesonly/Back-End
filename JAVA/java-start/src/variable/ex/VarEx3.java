eb413089e96f93e061b5479f043ff10442c0e215
ssunvibesonly <wlwlgoyo@naver.com>
2024-03-12 20:18:00 +0900

    240312

---
+++ JAVA/java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 10000000000L; //리터럴은 기본이 int형이므로 long은 리터럴 뒤에 L
+        System.out.println(l);
+
+        boolean b=true;
+        System.out.println(b);
+    }
+}
