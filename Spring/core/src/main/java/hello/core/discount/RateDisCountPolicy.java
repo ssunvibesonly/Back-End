f0ddbaede67d418ffc5d00013513bed6e792adfd
ssunvibesonly <wlwlgoyo@naver.com>
2024-03-12 13:24:04 +0900

    240312

---
+++ Spring/core/src/main/java/hello/core/discount/RateDisCountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDisCountPolicy implements  DiscountPolicy{
+
+    //VIP 등급인 경우 금액에 따라 10% 할인해준다.
+    private int percent=10;
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade()== Grade.VIP){
+            return price * percent/100;
+        }else{
+            return 0;
+        }
+    }
+}
--- Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDisCountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,13 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository=new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy=new FixDiscountPolicy(); => final은 무조건 값이 할당 되야 된다.
+    //private final DiscountPolicy discountPolicy=new RateDisCountPolicy();
+
+    //DIP(의존 관계 역전 원칙)를 위반하지 않도록 인터페이스에만 의존하도록 의존관계 변경
+    private  DiscountPolicy discountPolicy; //구체화에 의존하지 않고 추상화인 인터페이스에만 OrderServiceImpl이 의존
+
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member=memberRepository.findById(memberId);
