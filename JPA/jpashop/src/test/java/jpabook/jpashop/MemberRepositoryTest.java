15e7a7b1b023148ed878e41728ac8a0d49946b39
ssunvibesonly <wlwlgoyo@naver.com>
2024-03-20 16:26:36 +0900

    240320 mid

---
+++ JPA/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class) //junit한테 SpringBoot로 테스트 한다고 알려주는 것
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember(){
+        //given
+        Member member=new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveID= memberRepository.save(member);
+        Member findMember=memberRepository.find(saveID);
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
