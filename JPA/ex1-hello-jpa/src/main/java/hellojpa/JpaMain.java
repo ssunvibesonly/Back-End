4d0fc4aa359074b6924fd5c8d6ebca78d7476250
ssunvibesonly <wlwlgoyo@naver.com>
2024-03-26 11:23:18 +0900

    240326am

--- JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,21 +2,22 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
-
     public static void main(String[] args) {
-
-        EntityManagerFactory emf=Persistence.createEntityManagerFactory("hello");
-        EntityManager em=emf.createEntityManager();
-
-        EntityTransaction tx=em.getTransaction();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //모든 데이터를 변경하는 모든 작업은 JPA에서 꼭 transaction 단위 안에서 작업을 해야 한다.
+        EntityTransaction tx = em.getTransaction();
         tx.begin();
-        try {
+        try{
+            //비영속
             Member member=new Member();
-
-            member.setId(2L);
-            member.setName("helloB");
-
+            member.setId(100L);
+            member.setName("HelloJPA");
+            
+            //영속(이 때 DB에 저장되는 것이 아니다.)
             em.persist(member);
 
             tx.commit();
@@ -25,7 +26,6 @@ public class JpaMain {
         }finally {
             em.close();
         }
-
         emf.close();
     }
 }
