bb4f00b1de424e4c9f8e0a107497587bd4b62610
ssunvibesonly <wlwlgoyo@naver.com>
2024-03-27 14:24:53 +0900

    240327

--- JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,8 +12,30 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
+            Member member1=new Member();
+            member1.setUsername("A");
+
+            Member member2=new Member();
+            member2.setUsername("B");
+
+            Member member3=new Member();
+            member3.setUsername("C");
+
+            System.out.println("=======================================");
+
+            em.persist(member1); //1,51
+            em.persist(member2); //MEM
+            em.persist(member3); //MEM
+
+            System.out.println("member1 = " + member1.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
+
+            System.out.println("=======================================");
+
             tx.commit(); //commit 시점에 데이터베이스에 쿼리가 날아간다.
         }catch (Exception e){
+            e.printStackTrace();
             tx.rollback();
         }finally {
             em.close();
--- JPA/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -5,23 +5,35 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR",
+sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+initialValue = 1, allocationSize = 50 )
 public class Member {
-    @Id //PK매핑
+    @Id //직접 할당 : 직접 ID를 만들어서 할 당하는 경우 @Id
+    //@GeneratedValue IDENTITY 전략인 경우는 예외적이로 지연 쓰기 시점에 쿼리문이 DB로 날아간다.
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    @Column(name = "name") //컬럼명 변경
+    @Column(name = "name", nullable = false) //컬럼명 변경
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING) //enum타입을 쓰고 싶을 대
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob //DB에 varchar보다 큰 컨텐츠를 넣고 싶은 경우
-    private String description;
-    protected Member(){
-        //기본 생성자가 필요하다.
-        //: JPA의 구현체가 엔티티를 올바르게 인스턴스화하고 관리하기 위한 요구 사항 중 하나
+
+    public Member(){
+
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
