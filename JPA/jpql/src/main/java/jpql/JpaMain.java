a714a28065a77c173a5b37a0fa9d8dd9c31ca67d
ssunvibesonly <wlwlgoyo@naver.com>
2024-04-05 18:08:44 +0900

    240405 첫월급 받았당 ㅎㅎ

--- JPA/jpql/src/main/java/jpql/JpaMain.java
+++ JPA/jpql/src/main/java/jpql/JpaMain.java
@@ -13,19 +13,29 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+
+                Team team = new Team();
+                team.setName("teamA");
+                em.persist(team);
+
+                Member member = new Member();
+                member.setUsername("teamA");
+                member.setAge(10);
+
+                member.setTeam(team);
+
+                em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m")
-                            .getResultList();
-            Object[] result = resultList.get(0);
-            System.out.println("username = " + result[0]);
-            System.out.println("age = " + result[1]);
+            //left outer join에서 outer는 생략 가능
+            //left join에서 left빼면 내부조인
+            String query = "select m from Member m  join Team t on m.username=t.name";
+            List<Member> result= em.createQuery(query,Member.class)
+                                            .getResultList();
+
+            System.out.println("result = " + result.size());
 
             tx.commit();
         }catch (Exception e){
--- JPA/jpql/src/main/java/jpql/Member.java
+++ JPA/jpql/src/main/java/jpql/Member.java
@@ -9,9 +9,14 @@ public class Member {
     private Long id;
     private String username;
     private int age;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="TEAM_ID")
     private Team team;
+
+    public void changeTeam(Team team){
+        this.team=team;
+        team.getMembers().add(this);
+    }
 
     public Long getId() {
         return id;
@@ -35,5 +40,22 @@ public class Member {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
